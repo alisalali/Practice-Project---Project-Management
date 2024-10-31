99a3670b3b508a318b6e3543385cd2f117c010ca
alisalali <alis.alali@hotmail.com>
2024-10-31 22:27:23 +0300

    managing-state-to-switch-cmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handelStartProject() {
+    setProjectsState((prevProjects) => {
+      return {
+        ...prevProjects,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handelStartProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
+      <ProjectsSidebar onStartAddProject={handelStartProject} />
+      {content}
     </main>
   );
 }
